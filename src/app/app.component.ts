dfebbbc1fe03f3a3bf912c7b917170bfab3679d3
anudeepeloori <anudeepeloori@gmail.com>
2021-05-06 22:35:01 +0530

    commit3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  users=[];
-
- 
-  addUserData(ref){
-
-    let userObj=ref.value;
-    
-    //push into users array
-    this.users.push(userObj)
-
-    //clear form flields
-    ref.reset();
-  }
-   
-  deleteUser(index){
-    this.users.splice(index,1)
-  }
 
 
 }
