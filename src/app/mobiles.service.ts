7540ffa04ce33454236ff012676891df13e05109
anudeepeloori <anudeepeloori@gmail.com>
2021-05-22 22:30:14 +0530

    crud operations implemented

--- src/app/mobiles.service.ts
+++ src/app/mobiles.service.ts
@@ -15,8 +15,22 @@ export class MobilesService {
     //http get
    return this.hc.get<Mobile[]>("http://localhost:3000/mobiles")
 }
+
+//to save /create new mobile
   createNewMobile(mobileObj):Observable<any>{
   return this.hc.post("http://localhost:3000/mobiles",mobileObj)
   
 }
+
+//update mobile
+updateMobile(modifiedMobileObj):Observable<any>{
+  return this.hc.put("http://localhost:3000/mobiles/"+modifiedMobileObj.id,modifiedMobileObj)
+}
+
+ //delete mobile
+ deleteMobile(id):Observable<any>{
+  console.log("id is ",id)
+  return this.hc.delete("http://localhost:3000/mobiles/"+id)
+}
+
 }
